38fb621e409482e6582e03242b9dcf1c2e0ae644
Juliet <li_juliet@hotmail.com>
2021-10-24 20:27:08 -0400

    redo create, add edit in admin/trucksController

--- app/controllers/admin/trucks_controller.rb
+++ app/controllers/admin/trucks_controller.rb
@@ -10,11 +10,30 @@ class Admin::TrucksController < ApplicationController
 
   def create
     @truck = Truck.new(truck_params)
+    if @truck.save
+      redirect_to [:admin, :trucks], notice: 'Truck created!'
+    else
+      render :new
+    end
   end
+
+  def edit 
+    @truck = Truck.find(params[:id])
+  end
+
+  def update
+    @truck = Truck.find(params[:id])
+      if @truck.update(truck_params)
+        redirect_to @truck
+      else
+        render 'edit'
+      end
+    end
 
   def destroy
     @truck = Truck.find params[:id]
     @truck.destroy
+    redirect_to @truck
   end
 
   private
