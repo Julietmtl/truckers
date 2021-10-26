4b8126cdd4b9f8d77969a02b202383237993dc29
Juliet <li_juliet@hotmail.com>
2021-10-25 21:51:59 -0400

    add truck controller test

--- test/controllers/trucks_controller_test.rb
+++ test/controllers/trucks_controller_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class TrucksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get trucks_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get truck_url(@truck)
+    assert_response :success
+  end
 end
